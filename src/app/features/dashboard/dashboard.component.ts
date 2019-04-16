ca999c33157119e52d45b91dc84eb35eefd2f236
kyriakos13 <karageo133@gmail.com>
2019-04-17 01:44:11 +0300

    sorting paging test

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,23 +1,52 @@
 import { Component, OnInit } from '@angular/core';
 import { BugsService, Bug } from 'src/app/shared/common-services/bugs.service';
 
+export interface Pagination {
+  currentPage: 0;
+  itemsPerPage: 10;
+  totalItems: 10;
+  totalPages: number;
+}
+
+export class PaginatedResult<T> {
+  result: T;
+  pagination: Pagination;
+}
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-
   bugsList: Array<Bug> = []
+
+  genderList = [{value: 'male', display: 'Males'}, {value: 'female', display: 'Females'}];
+  userParams: any = {};
+  pagination: Pagination;
+
+  // bugsList: Array<Bug> = []
 
   constructor(private service: BugsService) { }
 
   ngOnInit() {
-    this.fetchBugs();
+    // this.fetchBugs();
+    this.loadUsers();
   }
 
   fetchBugs() {
     this.service.getBugsList().subscribe(data => this.bugsList = data);
+  }
+
+
+  loadUsers() {
+    this.service.getBugsListByParams(0, 10, this.userParams)
+      .subscribe((res: PaginatedResult<Bug[]>) => {
+        this.bugsList = res.result;
+        this.pagination = res.pagination;
+    }, error => {
+         console.log(error);
+    });
   }
 
 }
--- src/app/shared/common-services/bugs.service.ts
+++ src/app/shared/common-services/bugs.service.ts
@@ -1,21 +1,15 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient , HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment'
-
-@Injectable({
-  providedIn: 'root'
-})
-export class BugsService {
-
-  getAllBugsEndpoint = environment.endpointUrl + 'bugs';
-
-  constructor(private client: HttpClient) { }
-d
-  getBugsList(): Observable<Array<Bug>> {
-    return this.client.get<Array<Bug>>(this.getAllBugsEndpoint);
+import { map } from 'rxjs/operators';
+// import { Bug } from 'src/app/shared/common-services/bugs.service';
+export interface Comments {
+  id: string;
+  reporter: string;
+  description: string;
   }
-}
+
 
 export interface Bug {
   id: string;
@@ -29,8 +23,58 @@ export interface Bug {
   comments: Comments;
 }
 
-export interface Comments {
-  id: string;
-  reporter: string;
-  description: string;
+export interface Pagination {
+  currentPage: number;
+  itemsPerPage: number;
+  totalItems: number;
+  totalPages: number;
 }
+
+export class PaginatedResult<T> {
+  result: T;
+  pagination: Pagination;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BugsService {
+
+  getAllBugsEndpoint = environment.endpointUrl + 'bugs';
+
+  constructor(private http: HttpClient) { }
+d
+  getBugsList(): Observable<Array<Bug>> {
+    return this.http.get<Array<Bug>>(this.getAllBugsEndpoint);
+  }
+
+
+
+  getBugsListByParams(page?, itemsPerPage?, userParams?): Observable<PaginatedResult<Bug[]>> {
+    const paginatedResult: PaginatedResult<Bug[]> = new PaginatedResult<Bug[]>();
+
+    let params = new HttpParams();
+
+    if (page != null && itemsPerPage != null) {
+      params = params.append('page', page);
+      params = params.append('size', itemsPerPage);
+    }
+
+    if (userParams != null) {
+      // params = params.append('sort', userParams.orderBy);
+      params = params.append('sort', 'title,desc');
+    }
+
+    return this.http.get<Bug[]>(this.getAllBugsEndpoint, { observe: 'response', params})
+      .pipe(
+        map(response => {
+          paginatedResult.result = response.body;
+          if (response.headers.get('Pagination') != null) {
+            paginatedResult.pagination = JSON.parse(response.headers.get('Pagination'))
+          }
+          return paginatedResult;
+        })
+      );
+  
+
+      }}
